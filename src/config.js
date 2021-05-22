d0b6f1013cdb80ff4a3b3d181707f7c1935209d5
Michelle Dang <michelle.dang3929@gmail.com>
2021-05-22 00:11:42 -0400

    add config.js again

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export const authEndpoint = 'https://accounts.spotify.com/authorize';
+
+// Replace with your app's client ID, redirect URI and desired scopes
+export const clientId = '8286aa2886f744d09933877bc2490c40';
+export const redirectUri = 'http://localhost:3000/callback'; //http://michelledang.github.io/statify/
+export const scopes = ['user-top-read', 'user-read-private', 'user-read-email'];
