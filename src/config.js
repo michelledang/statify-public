bb34902df778832ace80c9e4bc7227394ce58ac3
Michelle Dang <michelle.dang3929@gmail.com>
2021-05-22 00:48:50 -0400

    remove gh-pages fixes

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ export const authEndpoint = 'https://accounts.spotify.com/authorize';
 
 // Replace with your app's client ID, redirect URI and desired scopes
 export const clientId = '8286aa2886f744d09933877bc2490c40';
-export const redirectUri = 'http://localhost:3000/callback'; //http://michelledang.github.io/statify/
+export const redirectUri = 'https://statify.vercel.app/callback'; //http://michelledang.github.io/statify/ http://localhost:3000/callback
 export const scopes = ['user-top-read', 'user-read-private', 'user-read-email'];
